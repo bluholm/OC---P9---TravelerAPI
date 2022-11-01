ffcc08b2b6c4c33520dc6367b39fc497ee6a4009
Mark <bar.marcantoine@gmail.com>
2022-11-01 19:04:04 +0800

    general change from notes from Valentin

---
+++ TravelerAPI/TravelerAPI/Currency/Controller/CurrencyController.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyController.swift
+//  TravelerAPI
+//
+//  Created by Marc-Antoine BAR on 2022-11-01.
+//
+
+import Foundation
---
+++ TravelerAPI/TravelerAPI/Currency/Controller/CurrencyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyViewController.swift
+//  TravelerAPI
+//
+//  Created by Marc-Antoine BAR on 2022-11-01.
+//
+
+import UIKit
+
+class CurrencyController: UIViewController {
+
+    // MARK: Life Cycle Method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
